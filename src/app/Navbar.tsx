d0ae43f86af193750571b6130840cab230f14619
fshojaei <fatemeeshojaeii@gmail.com>
2025-03-21 18:40:23 +0330

    fix stuff

---
+++ src/app/Navbar.tsx
@@ -0,0 +1,61 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import menuIcon from "@/public/menu.svg";
+import xmarkIcon from "@/public/xmark.svg";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuItems = [
+    { id: 1, name: "Home", link: "/" },
+    { id: 2, name: "My skills", link: "/skill" },
+    { id: 3, name: "About", link: "/about" },
+    { id: 4, name: "Experience", link: "/experience" },
+    { id: 5, name: "Contact", link: "/contact" },
+  ];
+  return (
+    <nav className="fixed w-full z-50">
+      <div className="container mx-auto flex justify-between items-center p-4">
+        <div className="text-xl font-bold">Fatemeh</div>
+
+        <ul className="hidden lg:flex space-x-10">
+          {menuItems.map((item) => (
+            <Link href={item.link} key={item.id}>
+              <li key={item.id} className="cursor-pointer">
+                {item.name}
+              </li>
+            </Link>
+          ))}
+        </ul>
+
+        <button className="lg:hidden" onClick={() => setIsOpen(true)}>
+          <Image src={menuIcon} alt="menu" width={20} height={20} />
+        </button>
+      </div>
+
+      {isOpen && (
+        <div className="fixed inset-0 bg-primary bg-opacity-90 flex flex-col items-center justify-center text-white text-2xl">
+          <button
+            className="absolute top-6 right-6"
+            onClick={() => setIsOpen(false)}
+          >
+            <Image src={xmarkIcon} alt="xmark" width={20} height={20} />
+          </button>
+
+          <ul className="space-y-10">
+            {menuItems.map((item) => (
+              <Link href={item.link} key={item.id}>
+                <li key={item.id} className="cursor-pointer">
+                  {item.name}
+                </li>
+              </Link>
+            ))}
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
