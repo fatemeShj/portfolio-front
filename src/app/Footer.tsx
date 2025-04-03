59ea1fa5f1a5e15acbe5d17987da91dde7fd5bc6
fshojaei <fatemeeshojaeii@gmail.com>
2025-04-03 14:50:25 +0330

    fix: fix footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -1,33 +1,46 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
 import linkedin from "@/public/linkedin.svg";
 import email from "@/public/email.svg";
 import github from "@/public/github.svg";
+import darkLinkedin from "@/public/dark-linkedin.svg";
+import darkEmail from "@/public/dark-email.svg";
+import darkGithub from "@/public/dark-github.svg";
 
 const Footer = () => {
+  const themeContext = useContext(ThemeContext);
+  const currentTheme = themeContext?.theme; // "dark-theme" or "light-theme"
+
   const socials = [
     {
       id: 1,
       name: "email",
       link: "mailto:fatemeeshojaeii@gmail.com",
-      icon: email,
+      icon: currentTheme === "dark-theme" ? email : darkEmail,
     },
     {
       id: 2,
       name: "github",
       link: "https://github.com/fatemeshj",
-      icon: github,
+      icon: currentTheme === "dark-theme" ? github : darkGithub,
     },
     {
       id: 3,
       name: "linkedin",
       link: "https://www.linkedin.com/in/fatemeh-shojaei-3929aa18a/",
-      icon: linkedin,
+      icon: currentTheme === "dark-theme" ? linkedin : darkLinkedin,
     },
   ];
 
   return (
-    <div className="flex justify-between items-center p-8 border-t border-tertiary mx-10 z-50">
+    <div
+      className={`flex justify-between items-center p-8 border-t mx-10 z-50 ${
+        currentTheme === "dark-theme" ? "border-tertiary" : "border-customGray"
+      }`}
+    >
       <div>Fatemeh Shojaei</div>
       <div className="flex space-x-4">
         {socials.map((social) => (
