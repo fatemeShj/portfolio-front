f4cc46a04319ac837d64f7f7c87e838b2fb42e88
fshojaei <fatemeeshojaeii@gmail.com>
2025-04-14 15:49:07 +0330

    fix stuff

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
-import darkMenuIcon from "@/assets/images/dark-menu.svg";
-import xmarkIcon from "@/assets/images/xmark.svg";
 import Link from "next/link";
+import Image from "next/image";
+import { useContext } from "react";
+import { ThemeContext } from "@/context/ThemeContext";
+import xmarkIcon from "@/assets/images/xmark.svg";
+import darkMenuIcon from "@/assets/images/dark-menu.svg";
+import lightMenuIcon from "@/assets/images/light-menu.svg";
+
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const themeContext = useContext(ThemeContext);
+  const currentTheme = themeContext?.theme;
   const menuItems = [
     { id: 1, name: "Home", link: "/" },
     { id: 2, name: "My skills", link: "/skill" },
@@ -30,9 +36,17 @@ const Navbar = () => {
           </ul>
           <ThemeSwitcher />
         </div>
-        <button className="lg:hidden" onClick={() => setIsOpen(true)}>
-          <Image src={darkMenuIcon} alt="menu" width={20} height={20} />
-        </button>
+        <div className="lg:hidden flex gap-5">
+          <ThemeSwitcher />
+          <button className="" onClick={() => setIsOpen(true)}>
+            <Image
+              src={currentTheme === "dark-theme" ? lightMenuIcon : darkMenuIcon}
+              alt="menu"
+              width={20}
+              height={20}
+            />
+          </button>
+        </div>
       </div>
 
       {isOpen && (
@@ -46,7 +60,11 @@ const Navbar = () => {
 
           <ul className="space-y-10">
             {menuItems?.map((item) => (
-              <Link href={item.link} key={item.id}>
+              <Link
+                href={item.link}
+                key={item.id}
+                onClick={() => setIsOpen(false)}
+              >
                 <li key={item.id} className="cursor-pointer">
                   {item.name}
                 </li>
